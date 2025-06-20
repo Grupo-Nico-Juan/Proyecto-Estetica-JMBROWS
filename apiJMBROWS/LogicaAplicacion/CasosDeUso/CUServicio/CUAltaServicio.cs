839374e57943dad17103149d4dbd96218eef6150
juan petit <juan.jose.petit@hotmail.com>
2025-06-20 15:08:52 -0300

    Se actualizo el CU alta servicio para modelar que un servicio se presta en una suscursal diferente y cada sector tiene una lista de servios

--- apiJMBROWS/LogicaAplicacion/CasosDeUso/CUServicio/CUAltaServicio.cs
+++ apiJMBROWS/LogicaAplicacion/CasosDeUso/CUServicio/CUAltaServicio.cs
@@ -1,3 +1,4 @@
+using LogicaAplicacion.Dtos.SectorDTO;
 using LogicaAplicacion.Dtos.ServicioDTO;
 using LogicaAplicacion.InterfacesCasosDeUso.ICUServicio;
 using LogicaNegocio.Entidades;
@@ -7,24 +8,41 @@ namespace LogicaAplicacion.CasosDeUso.CUServicio
 {
     public class CUAltaServicio : ICUAltaServicio
     {
-        private readonly IRepositorioServicios _repo;
+        private readonly IRepositorioServicios _repoServicios;
+        private readonly IRepositorioSectores _repoSectores;
 
-        public CUAltaServicio(IRepositorioServicios repo)
+        public CUAltaServicio(IRepositorioServicios repoServicios, IRepositorioSectores repoSectores)
         {
-            _repo = repo;
+            _repoServicios = repoServicios;
+            _repoSectores = repoSectores;
         }
 
         public void Ejecutar(AltaServicioDTO dto)
         {
+            var sector = _repoSectores.GetById(dto.SectorId);
+            if (sector == null)
+                throw new Exception("Sector no encontrado.");
+
+            if (sector.SucursalId != dto.SucursalId)
+                throw new Exception("El sector no pertenece a la sucursal indicada.");
+
             var nuevo = new Servicio
             {
                 Nombre = dto.Nombre,
                 Descripcion = dto.Descripcion,
                 DuracionMinutos = dto.DuracionMinutos,
-                Precio = dto.Precio
+                Precio = dto.Precio,
+                Sectores = new List<Sector> { sector }
             };
+
             nuevo.EsValido();
-            _repo.Add(nuevo);
+            _repoServicios.Add(nuevo);
         }
+
     }
 }
+
+
+
+
+
--- apiJMBROWS/LogicaAplicacion/Dtos/ServicioDTO/AltaServicioDTO.cs
+++ apiJMBROWS/LogicaAplicacion/Dtos/ServicioDTO/AltaServicioDTO.cs
@@ -7,6 +7,9 @@ namespace LogicaAplicacion.Dtos.ServicioDTO
         public int DuracionMinutos { get; set; }
         public decimal Precio { get; set; }
         // Si luego quieres asociar habilidades al crear, puedes agregar:
-        // public List<int> IdsHabilidades { get; set; }
+        //public List<int> IdsHabilidades { get; set; } = new();
+        //esto se pide solo para la validacion de la sucursal y sector
+        public required int SucursalId { get; set; }
+        public required int SectorId { get; set; }
     }
 }
