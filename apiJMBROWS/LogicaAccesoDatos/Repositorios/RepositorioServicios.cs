caf53b48dd71cf88a1bbbf732449b119e0b1f745
NicolasCabreraA <nicolascabreraaguilar@gmail.com>
2025-07-07 16:49:05 -0300

    correcciones cuservicios por sector y empleada

--- apiJMBROWS/LogicaAccesoDatos/Repositorios/RepositorioServicios.cs
+++ apiJMBROWS/LogicaAccesoDatos/Repositorios/RepositorioServicios.cs
@@ -30,6 +30,8 @@ namespace LogicaAccesoDatos.EF
         {
             return _context.Servicios
                 .Include(s => s.Extras)
+                .Include(s => s.Habilidades)
+                .Include(s => s.Sectores)
                 .ToList();
         }
 
